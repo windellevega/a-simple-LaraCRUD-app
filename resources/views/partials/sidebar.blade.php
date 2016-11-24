1419398a4cd0b00cb6ae595c44a7a7afd39f7598
Windelle John Vega <windellevega@gmail.com>
2016-11-24 07:47:41 +0000

    Update UI

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -6,8 +6,8 @@
     </form>
     <ul class="nav menu">
         <li id="dashboard"><a href="{{ url('/') }}"><svg class="glyph stroked dashboard-dial"><use xlink:href="#stroked-dashboard-dial"></use></svg> Dashboard</a></li>
-        <li id="tables"><a href="{{ route('record-list') }}"><svg class="glyph stroked table"><use xlink:href="#stroked-table"></use></svg> Tables</a></li>
-        <li id="forms"><a href="{{ route('record-add') }}"><svg class="glyph stroked pencil"><use xlink:href="#stroked-pencil"></use></svg> Forms</a></li>
+        <li id="tables"><a href="{{ route('record-list') }}"><svg class="glyph stroked table"><use xlink:href="#stroked-table"></use></svg> View Records</a></li>
+        <li id="forms"><a href="{{ route('record-add') }}"><svg class="glyph stroked pencil"><use xlink:href="#stroked-pencil"></use></svg> Add Record</a></li>
         <li role="presentation" class="divider"></li>
     </ul>
 
